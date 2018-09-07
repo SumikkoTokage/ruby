ea7852d1244e311138eff589c3426a90d8fa894b
田村真衣 <tamuramai@tamuramainoMacBook-puro.local>
2018-09-07 19:06:50 +0900

    [Finish]2-7

--- chapter7/lesson6.rb
+++ chapter7/lesson6.rb
@@ -1,1 +1,17 @@
-lesson6.rb
+
+
+total_price = 10
+
+if total_price == 100
+	# 100円だったら
+	puts "みかんを購入。所持金0円"
+
+elsif (total_price <= 100)
+	# 100円以下だったら
+	puts "みかんを購入することができません"
+
+else
+	# 100円以上だったら
+	puts "みかんを購入。所持金に余りあり"
+	
+end
